ed068d3db1008e33f6ed58659dd9aa4d8b2c29f5
Douglas Winter <douglaswinter75@gmail.com>
2024-02-08 18:53:56 -0300

    first commit

---
+++ src/Cliente.java
@@ -0,0 +1,8 @@
+public class Cliente {
+        public static void main(String args[]) {
+            int codigo = 10;
+            System.out.println(codigo);
+            Integer codigo1 = Integer.valueOf(codigo);
+            System.out.println(codigo1);
+        }
+}
---
+++ src/Transformer.java
@@ -0,0 +1,17 @@
+public class Transformer {
+
+        public static void main(String args[]) {
+            int num1 = 12;
+            System.out.println(num1);
+            long num2 = num1;
+            System.out.println(num2);
+
+            int num3 = 20000;
+            System.out.println(num3);
+            short num4 = (short) num3;
+            System.out.println(num4);
+
+        }
+
+    }
+
